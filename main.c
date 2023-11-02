6ae37260fc34c5011a69bd9b81cd89f5b55e159a
yannick-cousin1 <113202561+yannick-cousin1@users.noreply.github.com>
2023-11-02 16:33:43 +0100

    add comments to main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float determinant(float t[][2]);
+float determinant(float t[][2]);   //calcul du déterminant d'une matrice
 
 int main()
 {
@@ -11,7 +11,7 @@ int main()
     float x,y;
     int i,j;
     
-    for(i=0;i<2;i++)
+    for(i=0;i<2;i++)			// Saisie des coefficients du système
     {
 		for(j=0;j<2;j++)
 		{
@@ -20,19 +20,19 @@ int main()
 		}
 	}
 	
-	for(i=0;i<2;i++)
+	for(i=0;i<2;i++)		// Saisie des constantes du système
 	{
 		printf("Saisie de la constante [%d] : ", i);
 		scanf("%f", &cst[i]);
 	}
     
-    printf("\nRésolution du système :\n");
+    printf("\nRésolution du système :\n");		// Affichage du système d'équation
     printf("%f * X + %f * Y = %f\n", coeffs[0][0], coeffs[0][1], cst[0]);
     printf("%f * X + %f * Y = %f\n\n", coeffs[1][0], coeffs[1][1], cst[1]);
 
-    det = determinant(coeffs);
+    det = determinant(coeffs);			// Calcul du déterminant des coefficients
 
-    if( det != 0)
+    if( det != 0)					// Cas où 2 solutions existent
     {
         float Mx[2][2] = {{cst[0],coeffs[0][1]},{cst[1],coeffs[1][1]}};
         float My[2][2] = {{coeffs[0][0],cst[0]},{coeffs[1][0],cst[1]}};
@@ -43,11 +43,11 @@ int main()
         printf("\nx = %f\ny = %f\n", x, y);
     }
     
-    else
+    else	                           // Si le déterminent est nul
     {
         float Mx[2][2] = {{cst[0],coeffs[0][1]},{cst[1],coeffs[1][1]}};
         detx = determinant(Mx);
-        if (detx == 0)
+        if (detx == 0)				
         {
             printf("\nIl y a une infinité de solutions\n");
         }
@@ -62,5 +62,5 @@ int main()
 
 float determinant(float coeffs[][2])
 {
-    return (coeffs[0][0] * coeffs[1][1] - coeffs[0][1] * coeffs[1][0]);
+    return (coeffs[0][0] * coeffs[1][1] - coeffs[0][1] * coeffs[1][0]);  // a * d - b * c
 }
